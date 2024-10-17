61c58a593f633822d8925ec8abe2016a0975b4fe
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-10-17 20:54:15 +0600

    Different way a handle event in react

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+
+  function handleClick(){
+    alert('button click')
+  }
+
+  const handleClick2 = ()=>{
+    alert('Button two click')
+  }
+
+  const addToFive =(num) =>{
+    alert(num + 5)
+  }
+
+  return (
+    <>
+    
+      <h1>React Core Concept 2</h1>
+      <h3>Different Way To Handle Event</h3>
+      <button onClick={handleClick} >Click Me</button>
+      <button onClick={handleClick2}>Click 2</button>
+      <button onClick={ () => { alert('third button click') } }>Click 3</button>
+      <button onClick={ () => addToFive(3)}>Click 4</button>
+    </>
+  )
+}
+
+export default App
